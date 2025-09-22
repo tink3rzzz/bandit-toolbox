590592a3e679d54a9dea6b83e2d89c493cd3a6b0
tink3rzz <itsactuallydianairis@gmail.com>
2025-09-21 23:09:16 -0400

    Added lvl2 folder and script

---
+++ lvl2/lvl2.py
@@ -0,0 +1,21 @@
+import paramiko
+import os
+
+# SSH CONNECTION DETAILS
+host = "target_host"
+port = 2220
+user = "bandit1"
+password = os.getenv("BANDIT_PASS")
+
+# CREATE SSH CLIENT
+c = paramiko.SSHClient()
+c.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+try:
+    c.connect(host, port=port, username=user, password=password)
+    stdin, stdout, stderr = c.exec_command("cat ./-")
+    bandit2_pass = stdout.read().decode().strip()
+    print(bandit2_pass)
+
+finally:
+    c.close()
